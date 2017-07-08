b29faa748e1b86a60849fdf7bdcc3c7a64314906
Howard Grant <geovaniluigid@gmail.com>
2017-07-08 04:55:49 -0400

    Added Events form

---
+++ assets/js/frmHandler.js
@@ -0,0 +1,11 @@
+$(document).ready(function()
+{
+	//alert("Up and running!!");
+
+	//Temperary function
+	$(".events-button").click(function()
+	{
+		$("#frm-section").show();
+		window.location.href = "#topSec";
+	});
+});
